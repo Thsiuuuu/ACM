e304b542cf2fd8a49030aeebee2d5b9abac2d134
Thsiuuuu <czy0716@163.com>
2026-08-27 03:03:29 +0800

    2026.8.27 SAM板子

--- 字符串/后缀自动机/P3804 【模板】后缀自动机（SAM）.cpp
+++ 字符串/后缀自动机/P3804 【模板】后缀自动机（SAM）.cpp
@@ -18,8 +18,9 @@ using namespace std;
 #define VII vector<VI>
 #define VL  vector<ll>
 #define VLL vector<VL>
+const int Sig=26,MAXN=2e6+10;//maxn至少是字符串总长度2倍
+ll cnt[MAXN];
 struct SuffixAutomaton{
-    const int Sig=26,MAXN=2e6+10;//maxn至少是字符串总长度2倍
     struct Node{
         int len;
         int link;
@@ -27,18 +28,18 @@ struct SuffixAutomaton{
         Node():len(0),link(-1){
             memset(nxt,-1,sizeof(nxt));
         }
-    }t[N];
+    }t[MAXN];
     int tot,last;
     void init(){
         tot=1,last=0;
         t[0]=Node();
     }
-    void insert(int ch){
+    void insert(int c){
         int cur=tot++;
-        t[cut]=Node();
+        t[cur]=Node();
         t[cur].len=t[last].len+1;
         int p=last;
-        while(p!=-1&&!t[p].nxt[c]==-1){
+        while(p!=-1&&t[p].nxt[c]==-1){
             t[p].nxt[c]=cur;
             p=t[p].link;
         }
@@ -47,39 +48,42 @@ struct SuffixAutomaton{
         }else{
             int q=t[p].nxt[c];
             if(t[p].len+1==t[q].len){t[cur].link=q;}
-        }else{
-            int clone=tot++;
-            t[clone]=t[q];
-            t[clone].len=t[p].len+1;//接着原来那个点，然后把>x+1的点腾出来
-            while(p!=-1&&t[p].nxt[c]==q){
-                t[p].nxt[c]=clone;
-                p=t[p].link;
-            }//链上沿着s_i重连边
-            t[q].link=t[cur].link=clone;//两个状态都连到自己的link超集状态上面
+            else{
+                int clone=tot++;
+                t[clone]=t[q];
+                t[clone].len=t[p].len+1;//接着原来那个点，然后把>x+1的点腾出来
+                while(p!=-1&&t[p].nxt[c]==q){
+                    t[p].nxt[c]=clone;
+                    p=t[p].link;
+                }//链上沿着s_i重连边
+                t[q].link=t[cur].link=clone;//两个状态都连到自己的link超集状态上面
+            }
         }
         last=cur;
+        cnt[cur]=1;
     }
     void build(const string&s){
         init();
         for(char ch:s) insert(ch-'a');
     }
-    bool contain(const string&t){
-        int p=0;
-        for(char ch:t){
-            int c=ch-'a';
-            if(!t[p].nxt[c]) return false;
-            p=t[p].nxt[c];
-        }
-        return true;
-    }//是否包含子串
-    ll DisSubString(){
-        ll ans=0;
-        for(int p=1;p<tot;p++) ans+=t[p].len-t[t[p].link].len;
-        return ans;
+}S;
+
+VI e[MAXN];
+void dfs(ll&ans,int u){
+    for(int i:e[u]){
+        dfs(ans,i);
+        cnt[u]+=cnt[i];
     }
-};
+    if(u&&cnt[u]>1) ans=max(ans,cnt[u]*S.t[u].len);
+}
+
 void sol() {
-    
+    string s;cin>>s;
+    S.build(s);
+    for(int i=1;i<S.tot;i++) e[S.t[i].link].push_back(i);
+    ll ans=0;
+    dfs(ans,0);
+    cout<<ans;
 }
 
 signed main() {
@@ -87,7 +91,7 @@ signed main() {
     cin.tie(0), cout.tie(0);
 
     int t = 1;
-    cin >> t;
+    // cin >> t;
     while (t--) {
         sol();
     }
