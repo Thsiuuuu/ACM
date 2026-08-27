a44917ad9671cb70adcb7f9c7b0883a0a4c06033
Thsiuuuu <czy0716@163.com>
2026-08-27 12:30:18 +0800

    2026.8.27 SAM习题1

---
+++ 字符串/后缀自动机/SP8222 NSUBSTR - Substrings.cpp
@@ -0,0 +1,104 @@
+#include <bits/stdc++.h>
+using namespace std;
+/*
+      /\_/\
+     ( =o.o= ) *
+      / >  \>
+*/
+#define ll long long 
+#define i128 __int128_t
+#define u128 __uint128_t
+#define ld long double
+#define pii pair<int,int>
+#define pll pair<ll,ll>
+#define pil pair<int,ll>
+#define pli pair<ll,int>
+#define ull unsigned long long
+#define VI  vector<int>
+#define VII vector<VI>
+#define VL  vector<ll>
+#define VLL vector<VL>
+const int Sig=26,MAXN=5e5+10;//maxn至少是字符串总长度2倍
+ll sum[MAXN],ans[MAXN],cnt[MAXN],ord[MAXN]; 
+struct SuffixAutomaton{
+    struct Node{
+        int len;
+        int link;
+        int nxt[Sig];
+        Node():len(0),link(-1){
+            memset(nxt,-1,sizeof(nxt));
+        }
+    }t[MAXN];
+    int tot,last;
+    void init(){
+        tot=1,last=0;
+        t[0]=Node();
+    }
+    void insert(int c){
+        int cur=tot++;
+        t[cur]=Node();
+        t[cur].len=t[last].len+1;
+        int p=last;
+        while(p!=-1&&t[p].nxt[c]==-1){
+            t[p].nxt[c]=cur;
+            p=t[p].link;
+        }
+        if(p==-1){
+            t[cur].link=0;
+        }else{
+            int q=t[p].nxt[c];
+            if(t[p].len+1==t[q].len){t[cur].link=q;}
+            else{
+                int clone=tot++;
+                t[clone]=t[q];
+                t[clone].len=t[p].len+1;//接着原来那个点，然后把>x+1的点腾出来
+                while(p!=-1&&t[p].nxt[c]==q){
+                    t[p].nxt[c]=clone;
+                    p=t[p].link;
+                }//链上沿着s_i重连边
+                t[q].link=t[cur].link=clone;//两个状态都连到自己的link超集状态上面
+            }
+        }
+        last=cur;
+        sum[cur]=1;
+    }
+    void build(const string&s){
+        init();
+        for(char ch:s) insert(ch-'a');
+    }
+}S;
+void sol() {
+    string s;cin>>s;
+    S.build(s);
+    int n=s.size();
+    for(int i=0;i<S.tot;i++) cnt[S.t[i].len]++;
+    for(int i=1;i<=n;i++) cnt[i]+=cnt[i-1];
+    for(int i=S.tot-1;i>=0;i--){
+        int L=S.t[i].len;
+        ord[--cnt[L]]=i;
+    }
+    for(int i=S.tot-1;i;i--){
+        int u=ord[i];
+        int fa=S.t[u].link;
+        sum[fa]+=sum[u];
+    }
+    for(int i=S.tot-1;i>0;i--){
+        ans[S.t[i].len]=max(ans[S.t[i].len],sum[i]);
+    }
+    for(int i=n-1;i>0;i--){
+        ans[i]=max(ans[i+1],ans[i]);
+    }
+    for(int i=1;i<=n;i++) cout<<ans[i]<<'\n';
+}
+
+signed main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0), cout.tie(0);
+
+    int t = 1;
+    // cin >> t;
+    while (t--) {
+        sol();
+    }
+    return 0;
+}
